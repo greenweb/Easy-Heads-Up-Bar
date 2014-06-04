53c2ec8d8f6a626a3c04ad94386766bb54e03913
Rew Rixom - imac <rew@rixom.org>
2014-06-04 13:06:10 -0400

    Adding custom loop removed commented-out code
    Signed-off-by: Rew Rixom - imac <rew@rixom.org>

--- easy-heads-up-bar-v2/lib/ehb-frontend.php
+++ easy-heads-up-bar-v2/lib/ehb-frontend.php
@@ -6,19 +6,13 @@ class ehbFrontend
 {
   function __construct()
   {
-    // Hook into the 'init' action
-    #add_action( 'init', array($this,'ehb_add_post_type'), 0 );
-    // Help Bar
-    #add_action( 'contextual_help', array(&$this,'ehb_custom_help_tabs'), 10,3 );
-    // heads_up_bar post updated messages
-    #add_filter( 'post_updated_messages', array(&$this, 'ehb_updated_messages') );
     add_filter( 'the_content', array(&$this, 'ehu_show_bar') );
   }
 
   function ehu_show_bar()
   {
     $today = get_the_date();
-
+    $return = "";
     // WP_Query arguments
     $args = array(
       'post_type'              => 'heads_up_bar',
@@ -27,11 +21,19 @@ class ehbFrontend
 
     // The Query
     $query = new WP_Query( $args );
-
-    $return = "<pre>".print_r($query,true)."</pre>";
-    return $return;
+  // The Loop
+  if ( $query->have_posts() ) {
+    while ( $query->have_posts() ) {
+      $query->the_post();
+      $return .= get_the_title();
+    }
+  } else {
+    // no posts found
+  }
+  return $return;
+  // Restore original Post Data
+  wp_reset_postdata();
+   // $return = "<pre>".print_r($query,true)."</pre>";
   } // End fun ehu_fe_show_bar()
-
-  
 }
 $ehbFrontend = new ehbFrontend();
