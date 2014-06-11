165e504b361b6eac0409bb3bcf09243764036207
Rew Rixom - imac <rew@rixom.org>
2014-06-11 17:32:46 -0400

    removed commented out code
    Signed-off-by: Rew Rixom - imac <rew@rixom.org>

--- easy-heads-up-bar-v2/js/ehu.js
+++ easy-heads-up-bar-v2/js/ehu.js
@@ -1,19 +1,3 @@
-// // heads up bar
-// jQuery(document).ready(function($) {
-// 	$('div#ehu-bar').css({
-// 		'position': 'fixed',
-// 		'top': '0',
-// 		'left': '0',
-// 		'z-index' : 100001,
-// 		'width' : '100%'
-// 	});
-// 	$('div#ehu-bar').click(function () {
-// 		$(this).hide('slow', function() {
-			
-// 		});
-// 	});
-// });
-
 jQuery(document).ready(function($) {
 	var $ehuBar = $('div#ehu-bar');
 	var $ehuBarLinks = $('div#ehu-bar a');
