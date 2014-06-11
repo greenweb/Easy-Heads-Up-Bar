25602f25683d10d3cbca1771a0698c5b0609585d
Rew Rixom - imac <rew@rixom.org>
2014-06-11 17:34:16 -0400

    added js that hides and shows the bar
    Signed-off-by: Rew Rixom - imac <rew@rixom.org>

--- easy-heads-up-bar-v2/js/ehu.js
+++ easy-heads-up-bar-v2/js/ehu.js
@@ -6,9 +6,53 @@ jQuery(document).ready(function($) {
 	$barLocatoin = $ehuBar.attr('data-bar-location');
 	
 	if ($barLocatoin=='top') {
-		$ehuBar.prependTo('html');
+		$ehuBar.prependTo('body');
 	}else{
-				$ehuBar.appendTo('html');
+				$ehuBar.appendTo('body');
 	};
 	$ehuBarLinks.css({'color':$linkColor});
+
+	// close button
+	var $dhuCloseButton = $('#ehu-close-button');
+	var $dhuCloseButtonPosition = $dhuCloseButton.position();
+	
+	// Open button
+	var $dhuOpenButton = $('#ehu-open-button');
+	if ($barLocatoin=='top') 
+	{
+		$dhuOpenButton.css({
+			'top': $dhuCloseButtonPosition.top,	
+		});
+	}else{
+		$dhuOpenButton.css({
+			'bottom': $dhuCloseButtonPosition.bottom,	
+		})
+	};
+	$dhuOpenButton.css({
+		'right': $dhuCloseButtonPosition.right,	
+	});
+
+	// hide action
+	$dhuCloseButton.click(function() {
+	  $( this ).parent().slideUp( "fast", function() {
+	    	$dhuOpenButton.css({
+					'visibility': 'visible'	
+				});
+	  });
+	});
+
+	// hide action
+	$dhuOpenButton.click(function() {
+		$dhuOpenButton.css({'visibility': 'hidden'	});
+	  $ehuBar.slideDown( "fast", function() {
+			
+			if ($barLocatoin=='top') 
+			{
+				window.scrollTo(0,0);
+			}else{
+				window.scrollTo(0,$dhuCloseButtonPosition.top);
+			};
+	  });
+	});
+
 });
